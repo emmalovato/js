d6c7240f56afe90af9a066a674753063562ebc36
Emma Peterson <emmakpeterson@gmail.com>
2015-02-04 14:56:29 -0500

    build(dotfiles): add dotfiles for project config

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  var gulp = require('gulp'),
+      mocha = require('gulp-mocha'),
+      jshint = require('gulp-jshint'),
+      jscs = require('gulp-jscs'),
+      
+})();
+// we call it immediately
