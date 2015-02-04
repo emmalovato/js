cd3b7533562adb71ca3a4acf2bee727a7ed2ec2a
Emma Peterson <emmakpeterson@gmail.com>
2015-02-04 15:34:05 -0500

    build(gulpfile.js) created initial gulp file with mocha build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,13 @@
       mocha = require('gulp-mocha'),
       jshint = require('gulp-jshint'),
       jscs = require('gulp-jscs'),
-      
+      gulpUtil = require('gulp-util'),
+      stylish = require('jshint-stylish');
+
+  gulp.task('mocha', function() {
+    return gulp.src(['lib/**/*.spec.js', 'test/**/*.js'], {read: false})
+                .pipe(mocha({reporter: 'spec'})) // the reporter is the way mocha will display the test
+                .on('error', gulpUtil.log); // if we dont do this than gulp will close when an error is thrown 
+  });
 })();
-// we call it immediately
+// it's called immediately
